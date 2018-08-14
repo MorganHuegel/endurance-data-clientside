db1d504f0a2daeff66725b0ebcf847464e774ade
MorganHuegel <mhue2910@gmail.com>
2018-08-14 15:15:07 -0400

    styled deleteSingleWorkout component

--- src/components/workout-list/singleWorkoutDelete.js
+++ src/components/workout-list/singleWorkoutDelete.js
@@ -1,3 +1,5 @@
+import '../../stylesheets/workouts/singleWorkoutDelete.css';
+
 import React from 'react';
 import moment from 'moment';
 
@@ -5,15 +7,15 @@ export default function SingleWorkoutDelete(props){
   const workoutDate = props.currentWorkout.date;
 
   return(
-    <div>
-      <p>Delete workout from {moment(workoutDate).format('dddd, MMMM Do')} ?</p>
-      <div>
+    <div className='single-workout-delete'>
+      <p>Delete workout from <span className='date'>{moment(workoutDate).format('dddd, MMMM Do')}</span> ?</p>
+      <div className='button-container'>
 
-        <button onClick={() => props.confirmDelete()}>
+        <button onClick={() => props.confirmDelete()} className='delete button'>
           Confirm Delete
         </button>
 
-        <button onClick={() => props.toggleDeleteScreen(false)}>
+        <button onClick={() => props.toggleDeleteScreen(false)} className='back button'>
           Cancel
         </button>
       </div>
